6737316b80665a6e1557393faca1450198595754
Erik Hommen <hommenerik@gmail.com>
2016-05-26 21:02:39 -0400

    call response method

--- Shop_altered/js/main.js
+++ Shop_altered/js/main.js
@@ -6,7 +6,7 @@ $.ajax({
   type: 'GET',
   url: 'http://gateway.marvel.com:80/v1/public/characters?name=Thor&apikey=e1221702e8688a9361676710fc552a08'
 }).done(function(data){
-    console.log(data)
+    handleResponse(data);
 } )
 var handleResponse = function(takeinData) {
 	debugger;
