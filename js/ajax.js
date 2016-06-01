79de97f3d47a069282ae54589da4f51fbafbfc76
Erik Hommen <hommenerik@gmail.com>
2016-05-31 21:16:40 -0400

    last commit broke thor

--- js/ajax.js
+++ js/ajax.js
@@ -7,8 +7,7 @@ $.ajax({
   url: 'http://gateway.marvel.com:80/v1/public/characters?name=Thor&apikey=e1221702e8688a9361676710fc552a08'
 }).done(function(data){
     handleResponse(data);
-} );
-
+} )
 var handleResponse = function(takeinData) {
 	var extension = takeinData.data.results[0].thumbnail.extension;
 	$('#mySweetApiImage')[0].src = takeinData.data.results[0].thumbnail.path + '.' + extension;
