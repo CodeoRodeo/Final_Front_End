e85a358c68b885f762db6e87fa94e014365b2799
Erik Hommen <hommenerik@gmail.com>
2016-05-31 21:10:24 -0400

    trying a marvel console log

--- js/ajax.js
+++ js/ajax.js
@@ -7,7 +7,16 @@ $.ajax({
   url: 'http://gateway.marvel.com:80/v1/public/characters?name=Thor&apikey=e1221702e8688a9361676710fc552a08'
 }).done(function(data){
     handleResponse(data);
-} )
+} );
+
+marvel.characters.findAll(function(err, results) {
+  if (err) {
+    return console.error(err);
+  }
+ 
+  console.log(results);
+});
+
 var handleResponse = function(takeinData) {
 	var extension = takeinData.data.results[0].thumbnail.extension;
 	$('#mySweetApiImage')[0].src = takeinData.data.results[0].thumbnail.path + '.' + extension;
